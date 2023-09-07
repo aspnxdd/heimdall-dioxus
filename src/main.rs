541ea9e58e631fb8a9fc956adc60afcf731e9992
Arnau Espin <arnauespin@gmail.com>
2023-09-07 22:41:55 +0200

    fix: add static folder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod components;
 mod views;
 
+use std::path::PathBuf;
 use actix_web::{get, web, web::ServiceConfig, HttpResponse};
 use dioxus::prelude::*;
 use shuttle_actix_web::ShuttleActixWeb;
@@ -30,11 +31,13 @@ async fn decode_tx(path: web::Path<String>) -> HttpResponse {
 }
 
 #[shuttle_runtime::main]
-async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+async fn actix_web(
+    #[shuttle_static_folder::StaticFolder(folder = "public")] public_folder: PathBuf,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(decode_tx)
             .service(app_endpoint)
-            .service(actix_files::Files::new("/public", "./public"));
+            .service(actix_files::Files::new("/public", public_folder));
     };
     Ok(config.into())
 }
